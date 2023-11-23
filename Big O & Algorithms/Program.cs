0edeba192c43dca8a6b2c012a30dc4a9af47834d
returnaction <obergannv@gmail.com>
2023-11-23 15:51:07 -0500

    Add method check number for Even

--- Big O & Algorithms/Program.cs
+++ Big O & Algorithms/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1: Even or Odd
+
+            Console.WriteLine(CheckForEven(3));
+            Console.WriteLine(CheckForEven(2));
+        }
+
+        // Time complexity O(1) constant
+        public static bool CheckForEven(int number)
+        {
+            return number % 2 == 0 ? true : false;
         }
     }
 }
